e2b94b0bcd7104d2fb5f0951b418c645bffe140b
Stefano <sacchet13@gmail.com>
2023-02-04 12:05:36 +0100

    more comments on service slicing

--- Examples/serviceMigration/topology.py
+++ Examples/serviceMigration/topology.py
@@ -70,21 +70,17 @@ if __name__ == "__main__":
     net.addLinkNamedIfce(s1, h2, bw=1000, delay="5ms") #connect server to switch
     net.addLinkNamedIfce(s1, h3, bw=1000, delay="5ms") #connect server to switch
     # Add the interface for host internal traffic (connection used to migrate the server).
-    net.addLink(
-        s1, h2, bw=1000, delay="1ms", intfName1="s1-h2-int", intfName2="h2-s1-int"
-    )
-    net.addLink(
-        s1, h3, bw=1000, delay="1ms", intfName1="s1-h3-int", intfName2="h3-s1-int"
-    )
+    net.addLink(s1, h2, bw=1000, delay="1ms", intfName1="s1-h2-int", intfName2="h2-s1-int")
+    net.addLink(s1, h3, bw=1000, delay="1ms", intfName1="s1-h3-int", intfName2="h3-s1-int")
 
     info("\n*** Starting network\n")
     net.start()
 
-    s1_h1_port_num = get_ofport("s1-h1")
-    s1_h2_port_num = get_ofport("s1-h2")
-    s1_h3_port_num = get_ofport("s1-h3")
-    h2_mac = h2.MAC(intf="h2-s1")
-    h3_mac = h3.MAC(intf="h3-s1")
+    s1_h1_port_num = get_ofport("s1-h1") #port of the interface that connects the client
+    s1_h2_port_num = get_ofport("s1-h2") #port of the interface that connects the server
+    s1_h3_port_num = get_ofport("s1-h3") #port of the interface that connects the server1
+    h2_mac = h2.MAC(intf="h2-s1") #MAC address of the interface that connects the server
+    h3_mac = h3.MAC(intf="h3-s1") #MAC address of the interface that connects the server
 
     h2.setMAC("00:00:00:00:00:12", intf="h2-s1") #set the MAC address of the interface that connects to the switch.
     h3.setMAC("00:00:00:00:00:12", intf="h3-s1")
@@ -100,33 +96,34 @@ if __name__ == "__main__":
     # `ovs-ofctl` utility provided by the OvS.
     # For realistic setup, switches should be managed by a remote controller.
     info("*** Add flow to forward traffic from h1 to h2 to switch s1.\n")
+    #check_output runs a shell command and returns the stdout of the command as a byte string.
     check_output(
-        shlex.split(
+        shlex.split( #shlex.split is used to split the string into a list of arguments that can be passed to the check_output function.
             'ovs-ofctl add-flow s1 "in_port={}, actions=output:{}"'.format(
-                s1_h1_port_num, s1_h2_port_num
+                s1_h1_port_num, s1_h2_port_num #flow from h1 to h2
             )
         )
     )
     check_output(
         shlex.split(
             'ovs-ofctl add-flow s1 "in_port={}, actions=output:{}"'.format(
-                s1_h2_port_num, s1_h1_port_num
+                s1_h2_port_num, s1_h1_port_num #flow from h2 to h1
             )
         )
     )
     check_output(
         shlex.split(
             'ovs-ofctl add-flow s1 "in_port={}, actions=output:{}"'.format(
-                s1_h3_port_num, s1_h1_port_num
+                s1_h3_port_num, s1_h1_port_num #flow from h3 to h1
             )
         )
     )
 
-    info("*** h1 ping 10.0.0.12 with 3 packets: \n")
+    info("*** h1 ping 10.0.0.12 with 3 packets: \n") #ping from h1 to h2 ip
     ret = h1.cmd("ping -c 3 10.0.0.12")
     print(ret)
 
-    info("*** Deploy counter service on h2.\n")
+    info("*** Deploy counter service on h2.\n") #deploy server on h2
     counter_server_h2 = mgr.addContainer(
         "counter_server_h2", "h2", "service_migration", "python /home/server.py h2"
     )
@@ -139,24 +136,20 @@ if __name__ == "__main__":
     client_log = client_app.getLogs()
     print("\n*** Setup1: Current log of the client: \n{}".format(client_log))
 
-    info("*** Migrate (Re-deploy) the couter service to h3.\n")
+    info("*** Migrate (Re-deploy) the couter service to h3.\n") #migrate server from h2 to h3
     counter_server_h3 = mgr.addContainer(
-        "counter_server_h3",
-        "h3",
-        "service_migration",
-        "python /home/server.py h3 --get_state",
+        "counter_server_h3", "h3", "service_migration", "python /home/server.py h3 --get_state",
     )
-    info(
-        "*** Send SEGTERM signal to the service running on the h2.\n"
-        "Let it transfer its state through the internal network.\n"
+    info("*** Send SEGTERM signal to the service running on the h2.\n"
+         "Let it transfer its state through the internal network.\n"
     )
     pid_old_service = (
-        check_output(shlex.split("pgrep -f '^python /home/server.py h2$'"))
-        .decode("utf-8")
-        .strip()
+        check_output(shlex.split("pgrep -f '^python /home/server.py h2$'")) #check_outpu() runs a bash command and returns the output as a byte string
+        .decode("utf-8") #decode from bytes to string
+        .strip() #remove line breaks
     )
     for _ in range(3):
-        check_output(shlex.split("kill {}".format(pid_old_service)))
+        check_output(shlex.split("kill {}".format(pid_old_service))) #send the SIGTERM signal to the old service
         # Wait a little bit to let the signal work.
         time.sleep(1)
 
@@ -169,7 +162,7 @@ if __name__ == "__main__":
     check_output(
         shlex.split(
             'ovs-ofctl mod-flows s1 "in_port={}, actions=output:{}"'.format(
-                s1_h1_port_num, s1_h3_port_num
+                s1_h1_port_num, s1_h3_port_num #flow from h1 to h3
             )
         )
     )
@@ -180,10 +173,7 @@ if __name__ == "__main__":
 
     info("*** Migrate (Re-deploy) the couter service back to h2.\n")
     counter_server_h2 = mgr.addContainer(
-        "counter_server_h2",
-        "h2",
-        "service_migration",
-        "python /home/server.py h2 --get_state",
+        "counter_server_h2", "h2", "service_migration","python /home/server.py h2 --get_state",
     )
     pid_old_service = (
         check_output(shlex.split("pgrep -f '^python /home/server.py h3 --get_state$'"))
@@ -204,7 +194,7 @@ if __name__ == "__main__":
     check_output(
         shlex.split(
             'ovs-ofctl mod-flows s1 "in_port={}, actions=output:{}"'.format(
-                s1_h1_port_num, s1_h2_port_num
+                s1_h1_port_num, s1_h2_port_num #flow from h1 to h2
             )
         )
     )
@@ -213,7 +203,7 @@ if __name__ == "__main__":
     client_log = client_app.getLogs()
     print("\n*** Setup3: Current log of the client: \n{}".format(client_log))
 
-    if not AUTOTEST_MODE:
+    if not AUTOTEST_MODE: #if AUTOTEST_MODE = 0 give the terminal to the user
         CLI(net)
 
     try:
