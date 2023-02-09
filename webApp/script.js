8d7a4b689caa5b86b76ac01cfcc34137d4aa25a0
Saaam1607 <s.casagrandececchin@studenti.unitn.it>
2023-02-09 22:27:18 +0100

    aggiunto css e handler per invio

--- webApp/script.js
+++ webApp/script.js
@@ -1,12 +1,17 @@
+document.addEventListener("keydown", function(event) {
+    if (event.keyCode === 13) {
+        login()
+    }
+});
 
 function login(){
 
     username = document.getElementById("Username").value;
     password = document.getElementById("Password").value;
-    // console.log(username);
-    // console.log(password);
 
     if (username == "admin" && password == "admin"){
         window.location.href = "home.html";
+    } else{
+        alert("Errore: Username o Password errati")
     }
 }
