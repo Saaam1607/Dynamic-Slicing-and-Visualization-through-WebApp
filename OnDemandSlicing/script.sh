de58a17cf432207d4a7808adc8824ac25e57171c
vagrant <vagrant@comnetsemu>
2023-02-09 16:22:03 +0000

    updatedScript

--- OnDemandSlicing/script.sh
+++ OnDemandSlicing/script.sh
@@ -21,10 +21,8 @@ set port s2-eth1 qos=@newqos -- \
 set port s2-eth2 qos=@newqos -- \
 --id=@newqos create QoS type=linux-htb \
 other-config:max-rate=20000000 \
-queues:123=@1q \
-queues:234=@2q -- \
---id=@1q create queue other-config:min-rate=1000000 other-config:max-rate=5000000 -- \
---id=@2q create queue other-config:min-rate=1000000 other-config:max-rate=5000000
+queues:123=@1q -- \
+--id=@1q create queue other-config:min-rate=1000000 other-config:max-rate=5000000
 
 # S3
 echo 'Switch 3:'
