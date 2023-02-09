0c12ad2f3c4f4d40e1b749bd8c22c823d9e98ddf
Stefano <sacchet13@gmail.com>
2023-02-09 19:03:13 +0100

    fixed bug

--- webApp/index.js
+++ webApp/index.js
@@ -1,25 +1,19 @@
-const express = require("express");
-const app = express();
-const path = require("path");
+var http = require('http');
+var url = require('url');
+var fs = require('fs');
 
 PORT = 8080;
 
-app.get("/", function(req, res) {
-    var options = {
-        root: path.join(__dirname)
-    };
-     
-    var fileName = "index.html";
-    res.sendFile(fileName, options, function (err) {
-        if (err) {
-            next(err);
-        } else {
-            console.log('Sent:', fileName);
-        }
+http.createServer(function (req, res) {
+    var q = url.parse(req.url, true);
+    var filename = "." + q.pathname;
+    fs.readFile(filename, function(err, data) {
+      if (err) {
+        res.writeHead(404, {'Content-Type': 'text/html'});
+        return res.end("404 Not Found");
+      } 
+      res.writeHead(200, {'Content-Type': 'text/html'});
+      res.write(data);
+      return res.end();
     });
-  });
-
-app.listen(PORT, function(err) {
-    if (err) console.log(err);
-    console.log("Server listening on PORT", PORT);
-});
+  }).listen(8080);
